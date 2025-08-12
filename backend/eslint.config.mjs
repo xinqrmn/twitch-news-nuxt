08c95c58ef1a64ae6219518f48a44dbce92e0470
Ilya Shamin <tetrarch93@gmail.com>
2025-08-12 22:50:10 +0400

    Setup NestJS for development

--- backend/eslint.config.mjs
+++ backend/eslint.config.mjs
@@ -1,8 +1,7 @@
 // @ts-check
-import eslint from '@eslint/js';
-import eslintPluginPrettierRecommended from 'eslint-plugin-prettier/recommended';
-import globals from 'globals';
-import tseslint from 'typescript-eslint';
+import eslint from '@eslint/js'
+import globals from 'globals'
+import tseslint from 'typescript-eslint'
 
 export default tseslint.config(
   {
@@ -10,7 +9,6 @@ export default tseslint.config(
   },
   eslint.configs.recommended,
   ...tseslint.configs.recommendedTypeChecked,
-  eslintPluginPrettierRecommended,
   {
     languageOptions: {
       globals: {
@@ -28,7 +26,8 @@ export default tseslint.config(
     rules: {
       '@typescript-eslint/no-explicit-any': 'off',
       '@typescript-eslint/no-floating-promises': 'warn',
-      '@typescript-eslint/no-unsafe-argument': 'warn'
+      '@typescript-eslint/no-unsafe-argument': 'warn',
+      semi: 'off',
     },
-  },
-);
+  }
+)
--- backend/src/main.ts
+++ backend/src/main.ts
@@ -1,8 +1,27 @@
-import { NestFactory } from '@nestjs/core';
-import { AppModule } from './app.module';
+import { NestFactory } from '@nestjs/core'
+import { AppModule } from './app.module'
+import helmet from 'helmet'
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger'
+import { ConfigService } from '@nestjs/config'
+import { IApp } from './common/configuration'
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
-  await app.listen(process.env.PORT ?? 3000);
+  const app = await NestFactory.create(AppModule)
+
+  const config = app.get(ConfigService).getOrThrow<IApp>('app')
+
+  const documentationConfig = new DocumentBuilder()
+    .setTitle('Twitch-News-Nest')
+    .setDescription('API Twitch News')
+    .setVersion('3.0')
+    .build()
+  const document = SwaggerModule.createDocument(app, documentationConfig)
+  SwaggerModule.setup('docs', app, document)
+
+  app.use(helmet())
+  await app.listen(config.port, config.address, () =>
+    console.info(`Server started on port: ${config.port} in ${process.env.NODE_ENV} mode`),
+  );
 }
-bootstrap();
+// eslint-disable-next-line
+bootstrap()
