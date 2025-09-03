92dc94a7bbc7b7218a6c6285a12e038d6282b627
Roman Galanov <mongo4558@gmail.com>
2025-09-03 17:53:05 +0400

    Created Topbar and sidebar navigation

--- cms/src/pages/Dashboard.vue
+++ cms/src/pages/Dashboard.vue
@@ -3,6 +3,8 @@ import { useAuthStore } from '@/stores/auth'
 import Button from 'primevue/button'
 import Card from 'primevue/card'
 import { useRouter } from 'vue-router'
+import AppTopbar from '@/layout/AppTopbar.vue'
+import AppSidebar from '@/layout/AppSidebar.vue'
 
 const router = useRouter()
 const authStore = useAuthStore()
@@ -14,13 +16,17 @@ const logout = () => {
 </script>
 
 <template>
-  <div class="p-6 bg-gray-50 min-h-screen">
-    <Card>
-      <template #title>Панель управления</template>
-      <template #content>
-        <p>Добро пожаловать в вашу CMS!</p>
-        <Button label="Выйти" class="mt-4" @click="logout" />
-      </template>
-    </Card>
+  <div class="bg-gray-50 dark:bg-surface-950 min-h-screen">
+    <AppTopbar></AppTopbar>
+    <AppSidebar></AppSidebar>
+    <div class="ml-[22rem] pt-[6rem] pr-8 pl-8">
+      <Card class="">
+        <template #title>Панель управления</template>
+        <template #content>
+          <p>Добро пожаловать в вашу CMS!</p>
+          <Button label="Выйти" class="mt-4" @click="logout" />
+        </template>
+      </Card>
+    </div>
   </div>
 </template>
--- cms/src/stores/auth.ts
+++ cms/src/stores/auth.ts
@@ -1,20 +1,26 @@
 import { defineStore } from 'pinia'
+import { ref } from 'vue'
 
-export const useAuthStore = defineStore('auth', {
-  state: () => ({
-    isAuthenticated: false,
-  }),
-  actions: {
-    login(username: string, password: string) {
-      // Здесь будет реальный API вызов
-      if (username === 'admin' && password === '1234') {
-        this.isAuthenticated = true
-        return true
-      }
-      return false
-    },
-    logout() {
-      this.isAuthenticated = false
-    },
-  },
+export const useAuthStore = defineStore('auth', () => {
+  const isAuthenticated = ref<boolean>(localStorage.getItem('isAuth') === 'true')
+
+  const login = (username: string, password: string): boolean => {
+    if (username === 'admin' && password === '1234') {
+      isAuthenticated.value = true
+      localStorage.setItem('isAuth', 'true')
+      return true
+    }
+    return false
+  }
+
+  const logout = (): void => {
+    isAuthenticated.value = false
+    localStorage.setItem('isAuth', 'false')
+  }
+
+  return {
+    isAuthenticated,
+    login,
+    logout,
+  }
 })
