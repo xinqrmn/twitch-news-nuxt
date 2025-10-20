cf8fb9f94fe03ca11f794a33353a9611b1fcfdc0
Ilya Shamin <tetrarch93@gmail.com>
2025-10-20 15:55:37 +0400

    Fix: slight changes to migrations

--- backend/src/common/data-source.ts
+++ backend/src/common/data-source.ts
@@ -6,14 +6,12 @@ config({path: '.env'})
 
 export const AppDataSource = new DataSource({
   type: 'postgres',
-	host: process.env.POSTGRES_HOST ?? 'localhost',
-	port: Number.parseInt(process.env.POSTGRES_PORT ?? '5432'),
-	username: process.env.POSTGRES_USERNAME ?? 'user',
-	password: process.env.POSTGRES_PASSWORD ?? 'password',
-	database: process.env.POSTGRES_DATABASE ?? 'postgres',
-  entities: ['src/**/*.entity.ts'],
-  migrations: ['src/migrations/*.ts'],
+  host: process.env.POSTGRES_HOST ?? 'localhost',
+  port: Number.parseInt(process.env.POSTGRES_PORT ?? '5432'),
+  username: process.env.POSTGRES_USERNAME ?? 'user',
+  password: process.env.POSTGRES_PASSWORD ?? 'password',
+  database: process.env.POSTGRES_DATABASE ?? 'postgres',
+  entities: ['dist/**/*.entity{.ts,.js}'],
+  migrations: ['src/migrations/*{.ts,.js}'],
   namingStrategy: new SnakeNamingStrategy(),
-  synchronize: false,
-  logging: true,
 })
